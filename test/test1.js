0f5e05527a0c2d21d2d2eb63adf2a659b036d8d0
Annnia20 <70884732+Annnia20@users.noreply.github.com>
2022-01-13 15:40:07 +0200

    change test result

--- test/test1.js
+++ test/test1.js
@@ -3,13 +3,13 @@ const expect = require('chai').expect;
 const math = require('../math');
 describe('math.js tests', () => {
     describe('math.add() Test', () => {
-        it('should equal 2', () => {
-            const result = math.add(1, 1);
-            expect(result).to.equal(2);
+        it('should equal 10', () => {
+            const result = math.add(6, 4);
+            expect(result).to.equal(10);
         });
-        it('should equal 4', () => {
-            const result = math.add(2, 2);
-            expect(result).to.equal(4);
+        it('should equal 6', () => {
+            const result = math.add(4, 2);
+            expect(result).to.equal(6);
         });
     });
 
