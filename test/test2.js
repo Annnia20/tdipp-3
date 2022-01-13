b59eb5827a0a12344548021966fdcd74034085d0
Annnia20 <70884732+Annnia20@users.noreply.github.com>
2022-01-13 15:42:22 +0200

    update the results

--- test/test2.js
+++ test/test2.js
@@ -4,13 +4,13 @@ const math = require('../math');
 describe('math.js tests', () => {
     
     describe('math.multiply() Test', () => {
-        it('should equal 3', () => {
-            const result = math.multiply(3, 1);
-            expect(result).to.equal(3);
+        it('should equal 6', () => {
+            const result = math.multiply(2, 3);
+            expect(result).to.equal(6);
         });
-        it('should equal 10', () => {
-            const result = math.multiply(5, 2);
-            expect(result).to.equal(10);
+        it('should equal 12', () => {
+            const result = math.multiply(4, 3);
+            expect(result).to.equal(12);
         });
     });
 });
